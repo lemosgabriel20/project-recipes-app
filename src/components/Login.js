1f7a459f4416140d6a9ab1a8d1caa6d9f4b1a0f7
Gabriel Lemos <jmjgb@outlook.com>
2023-03-02 16:19:45 -0300

    Checagem de validação do email e senha e testes de cobertura no Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import isEmail from 'validator/lib/isEmail';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [passwordLen, setPasswordLen] = useState('');
+  const minLength = 6;
   return (
     <div>
-      <input
-        type="email"
-        data-testid="email-input"
-      />
-      <input
-        type="password"
-        data-testid="password-input"
-      />
-      <input
-        type="button"
-        data-testid="login-submit-btn"
-      />
+      <form>
+        <input
+          type="email"
+          data-testid="email-input"
+          onChange={ (evt) => setEmail(evt.target.value) }
+          required
+        />
+        <input
+          type="password"
+          minLength="6"
+          onChange={ (evt) => setPasswordLen((evt.target.value).length) }
+          required
+          data-testid="password-input"
+        />
+        <input
+          type="submit"
+          data-testid="login-submit-btn"
+          disabled={ !isEmail(email) || (passwordLen <= minLength) }
+        />
+      </form>
     </div>
   );
 }
