2a1838ef326e28ff1d12ee03cb00c0ee91399ac8
rafaelaxgranato <rafaelaxgranato@gmail.com>
2023-03-02 15:25:27 -0300

    iniciando o projeto

--- src/index.js
+++ src/index.js
@@ -12,5 +12,3 @@ ReactDOM
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-// iniciando o projeto
