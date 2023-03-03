e8eafaa3a90b03b0590b9649b0da56ee364d284c
Gabriel Lemos <jmjgb@outlook.com>
2023-03-03 00:41:56 -0300

    Lógica dos radio buttons implementada e call para API

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function SearchBar() {
+  const [search, setSearch] = useState('');
+  const [endpoint, setEndpoint] = useState('');
+
+  const checkEndpoint = () => {
+    if (endpoint === 'ingredient') return `https://www.themealdb.com/api/json/v1/1/filter.php?i=${search}`;
+    if (endpoint === 'name') return `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`;
+    if (endpoint === 'letter') return `https://www.themealdb.com/api/json/v1/1/search.php?f=${search}`;
+  };
+
+  const fetchApi = async () => {
+    const url = checkEndpoint();
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.meals);
+  };
+
   return (
     <div>
       <label>
@@ -8,6 +24,7 @@ export default function SearchBar() {
           data-testid="search-input"
           type="text"
           name="search"
+          onChange={ (evt) => setSearch(evt.target.value) }
         />
       </label>
       <label>
@@ -15,6 +32,7 @@ export default function SearchBar() {
           data-testid="ingredient-search-radio"
           type="radio"
           name="search"
+          onClick={ () => setEndpoint('ingredient') }
         />
         Ingredient
       </label>
@@ -23,6 +41,7 @@ export default function SearchBar() {
           data-testid="name-search-radio"
           type="radio"
           name="search"
+          onClick={ () => setEndpoint('name') }
         />
         Name
       </label>
@@ -31,11 +50,19 @@ export default function SearchBar() {
           data-testid="first-letter-search-radio"
           type="radio"
           name="search"
+          onClick={ () => setEndpoint('letter') }
         />
         First letter
       </label>
       <button
         data-testid="exec-search-btn"
+        disabled={ endpoint === '' }
+        onClick={ () => {
+          if (endpoint === 'letter' && search.length > 1) {
+            global.alert('Your search must have only 1 (one) character');
+          }
+          fetchApi();
+        } }
       >
         Search
       </button>
