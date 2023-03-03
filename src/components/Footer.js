7f3416bfa746a1aed814ee4e57b050763b650fa0
rafaelaxgranato <rafaelaxgranato@gmail.com>
2023-03-03 07:55:16 -0300

    menu inferior e testes

---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+
+export default function Footer() {
+  const history = useHistory();
+  return (
+    <div
+      style={ { position: 'fixed' } }
+      data-testid="footer"
+    >
+      <button onClick={ () => history.push('/drinks') }>
+        <img
+          data-testid="drinks-bottom-btn"
+          src={ drinkIcon }
+          alt="Drink"
+        />
+      </button>
+      <button onClick={ () => history.push('/meals') }>
+        <img
+          data-testid="meals-bottom-btn"
+          src={ mealIcon }
+          alt="Meal"
+        />
+      </button>
+    </div>
+  );
+}
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Profile() {
@@ -10,6 +11,7 @@ export default function Profile() {
       <button type="button" onClick={ () => history.push('/done-recipes') }>
         Done Recipes
       </button>
+      <Footer />
     </div>
   );
 }
