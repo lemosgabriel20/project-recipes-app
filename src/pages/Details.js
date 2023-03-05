930d8ea81ffaf15dad4f6c0dc8b709a253442fb9
Gabriel Lemos <jmjgb@outlook.com>
2023-03-05 05:53:46 -0300

    Detalhes das comidas e bebidas renderizados

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -2,21 +2,84 @@ import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 export default function Details() {
-  const [recipe, setRecipe] = useState();
+  const [recipe, setRecipe] = useState(null);
+  const [ingredients, setIngredients] = useState([]);
+  const [measures, setMeasures] = useState([]);
   const { pathname } = useLocation();
   const params = pathname.slice(1).split('/');
   const token = params[0];
   const id = params[1];
   const web = (token.includes('meals')) ? 'themealdb' : 'thecocktaildb';
+  const image = (token.includes('meals')) ? 'strMealThumb' : 'strDrinkThumb';
+  const name = (token.includes('meals')) ? 'strMeal' : 'strDrink';
+  const category = (token.includes('meals')) ? 'strCategory' : 'strAlcoholic';
+  const instructions = 'strInstructions';
+  const youtube = 'strYoutube';
+
   useEffect(() => {
     const fetchById = async () => {
       const response = await fetch(`https://www.${web}.com/api/json/v1/1/lookup.php?i=${id}`);
       const data = await response.json();
-      setRecipe(data[token]);
+      setRecipe(data[token][0]);
     };
     fetchById();
   }, [id, token, web]);
-  return (
-    <div>{console.log(recipe)}</div>
-  );
+
+  useEffect(() => {
+    if (recipe) {
+      const fetchCategories = async () => {
+        const response = await fetch(`https://www.${web}.com/api/json/v1/1/lookup.php?i=${id}`);
+        const data = await response.json();
+        const dataFormatted = data[token][0];
+        const keys = Object.keys(dataFormatted);
+        const values = Object.values(dataFormatted);
+        const ingredientsResult = values.filter((ingredient, index) => {
+          if (keys[index].includes('strIngredient')) {
+            return ingredient;
+          }
+          return '';
+        });
+        const measureResult = values.filter((measure, index) => {
+          if (keys[index].includes('strMeasure')) {
+            return measure;
+          }
+          return '';
+        });
+        setIngredients(ingredientsResult);
+        setMeasures(measureResult);
+      };
+      fetchCategories();
+    }
+  }, [recipe, token, id, web]);
+
+  if (recipe) {
+    return (
+      <div>
+        <img width="200px" data-testid="recipe-photo" src={ recipe[image] } alt="" />
+        <h1 data-testid="recipe-title">{ recipe[name] }</h1>
+        <h4 data-testid="recipe-category">{ recipe[category] }</h4>
+        {
+          ingredients.map((ingredient, index) => {
+            const key = index;
+            return (
+              <div
+                key={ key }
+                data-testid={ `${key}-ingredient-name-and-measure` }
+              >
+                { `${ingredient} ${measures[key]}` }
+              </div>
+            );
+          })
+        }
+        <p data-testid="instructions">{ recipe[instructions] }</p>
+        <iframe
+          data-testid="video"
+          title="Video"
+          src={ recipe[youtube] }
+          frameBorder="0"
+          allowFullScreen
+        />
+      </div>
+    );
+  }
 }
