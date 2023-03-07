5852899f19aba7029fa94a424aaefb9cabad94af
Gabriel Lemos <jmjgb@outlook.com>
2023-03-07 14:46:12 -0300

    Aprensetação dos botões e elementos da DoneRecipes

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,10 +1,80 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 
 export default function DoneRecipes() {
-  return (
-    <div>
-      <Header />
-    </div>
-  );
+  const [recipes, setRecipes] = useState(JSON.parse(localStorage.getItem('doneRecipes')));
+
+  useEffect(() => {
+    const local = JSON.parse(localStorage.getItem('doneRecipes'));
+    if (local !== undefined) {
+      setRecipes(recipes);
+      console.log(recipes);
+    }
+  }, [recipes]);
+
+  if (recipes !== undefined) {
+    return (
+      <div>
+        <Header />
+        <button
+          data-testid="filter-by-all-btn"
+        >
+          All
+        </button>
+        <button
+          data-testid="filter-by-meal-btn"
+        >
+          Meals
+        </button>
+        <button
+          data-testid="filter-by-drink-btn"
+        >
+          Drinks
+        </button>
+        {
+          recipes.map((recipe, index) => {
+            const key = index;
+            const tags = [...recipe.tags];
+            return (
+              <div key={ key }>
+                <img
+                  src={ recipe.image }
+                  alt=""
+                  data-testid={ `${index}-horizontal-image` }
+                  width="200px"
+                />
+                <h1 data-testid={ `${index}-horizontal-name` }>{recipe.name}</h1>
+                <h2
+                  data-testid={ `${index}-horizontal-top-text` }
+                >
+                  {recipe.category}
+                </h2>
+                <h3
+                  data-testid={ `${index}-horizontal-done-date` }
+                >
+                  {recipe.doneDate}
+                </h3>
+                <button
+                  data-testid={ `${index}-horizontal-share-btn` }
+                >
+                  Share
+                </button>
+                {
+                  (tags).map((tag, i) => {
+                    const k = i;
+                    return (
+                      <div
+                        key={ k }
+                        data-testid={ `${index}-${tag}-horizontal-tag` }
+                      />
+                    );
+                  })
+                }
+              </div>
+            );
+          })
+        }
+      </div>
+    );
+  }
 }
